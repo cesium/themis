1030d91ec3b8202190a36e807da10c780098fed7
Gerson Junior <ger_son_junior@live.com.pt>
2023-01-28 14:39:07 +0000

    Add font in tailwind.config.js and remove unnecessary code

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
   theme: {
     fontFamily:{
       iblack: ["Rokkitt-Black"],
-        iextrabold: ["Rokkitt-ExtraBold"],
-        ibold: ["Rokkitt-Bold"],
-        isemibold: ["Rokkitt-SemiBold"],
-        imedium: ["Rokkitt-Medium"],
-        iregular: ["Rokkitt-Regular"],
-        ilight: ["Inter-Light"],
-        iextralight: ["Rokkitt-ExtraLight"],
-        ithin: ["Rokkitt-Thin"],
+      iextrabold: ["Rokkitt-ExtraBold"],
+      ibold: ["Rokkitt-Bold"],
+      isemibold: ["Rokkitt-SemiBold"],
+      imedium: ["Rokkitt-Medium"],
+      iregular: ["Rokkitt-Regular"],
+      ilight: ["Inter-Light"],
+      iextralight: ["Rokkitt-ExtraLight"],
+      ithin: ["Rokkitt-Thin"],
     }
   },
   plugins: [],
