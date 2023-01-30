5d9005f5794c26c2924ba90a7bb398d8466f1549
Gerson Junior <ger_son_junior@live.com.pt>
2023-01-30 15:33:32 +0000

    Rename some variables

--- src/components/Organization.jsx
+++ src/components/Organization.jsx
@@ -4,7 +4,7 @@ import { Container } from '@/components/Container'
 import logoCesium from '@/images/organization/CeSIUM-icon.svg'
 import logoELSA from '@/images/organization/ELSA.png'
 
-const organization = [
+const organizers = [
   { name: 'CeSIUM', logo: logoCesium },
   { name: 'ELSA', logo: logoELSA },
 ]
@@ -17,9 +17,9 @@ export function Organization() {
             Organizadores
         </h2>
         <div className=" flex flex-row items-center gap-16 mx-auto">
-          {organization.map((sponsor) => (
-            <div key={sponsor.name}>
-              <Image className="" src={sponsor.logo} alt={sponsor.name} height={80} unoptimized />
+          {organizers.map((organization) => (
+            <div key={organization.name}>
+              <Image className="" src={organization.logo} alt={organization.name} height={80} unoptimized />
             </div>
           ))}
         </div>
