59a0fa72ea0069f7d7e727e53050fb20403831fe
Gerson Junior <ger_son_junior@live.com.pt>
2023-03-01 17:20:47 +0000

    Add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import { DiamondIcon } from '@/components/DiamondIcon'
-import { Logo } from '@/components/Logo'
+import Logo from 'public/images/logoA.svg'
+import Image from 'next/image'
+
 
 export function Header() {
   return (
     <header className="relative z-50 pb-11 lg:pt-11">
       <Container className="flex flex-wrap items-center justify-center sm:justify-between lg:flex-nowrap">
         <div className="mt-10 lg:mt-0 lg:grow lg:basis-0">
-          <Logo className="h-12 w-auto text-slate-900" />
+          <Image
+            className="h-12 w-auto text-slate-900"
+            src={Logo}
+            alt=""
+            width={50}
+            height={25}
+            priority
+            unoptimized
+          />
         </div>
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-primary/10 py-4 font-mono text-sm text-primary sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
           <div className="mx-auto flex items-center gap-4 px-4">
