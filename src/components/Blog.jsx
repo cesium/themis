eb9d032935f238c6c5f51f3978bba63412f841d9
Gerson Junior <ger_son_junior@live.com.pt>
2023-03-06 18:08:37 +0000

    Remove function not used

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,21 +4,6 @@ import { Container } from '@/components/Container'
 import backgroundImage from 'public/images/background.jpg'
 import writers from '@/data/blog.json'
 
-function ArrowRightIcon(props) {
-  return (
-    <svg aria-hidden="true" viewBox="0 0 24 24" {...props}>
-      <path
-        d="m14 7 5 5-5 5M19 12H5"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
-}
-
 function PaperStatic() {
   return (
     <div className="flex flex-col mt-20 lg:mx-36 rounded-md">
