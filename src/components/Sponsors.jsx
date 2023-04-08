5821d0cb41870e6ba1da0960c773e8c7bfc3d16a
Gerson Junior <58490266+GresosJ@users.noreply.github.com>
2023-04-08 17:23:45 +0100

    Update src/components/Sponsors.jsx
    Co-authored-by: Pedro Pereira <109802203+pedrofp4444@users.noreply.github.com>

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -15,7 +15,7 @@ export function Sponsors() {
     >
       <Container>
         <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-jordi-orange-500 sm:text-5xl">
-          Sponsorships
+          Sponsorship
         </h2>
         <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-y-12 gap-x-32 md:gap-x-16 lg:gap-x-32">
           {sponsors.map((sponsor) => (
