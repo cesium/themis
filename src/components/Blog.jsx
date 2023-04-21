96a17e25ec2fc2808bdbc9931ab901e3fde35c44
Gerson Junior <ger_son_junior@live.com.pt>
2023-04-21 17:21:18 +0100

    Remove not used function

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -6,21 +6,6 @@ import React, { useState } from 'react'
 import backgroundImage from 'public/images/themis.svg'
 import writers from '@/data/blog.json'
 
-function ArrowRightIcon(props) {
-  return (
-    <svg aria-hidden="true" viewBox="0 0 24 24" {...props}>
-      <path
-        d="m14 7 5 5-5 5M19 12H5"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
-}
-
 function ShowPaper({ paper }) {
   return (
     <div className="mb-6 space-y-8 rounded-2xl bg-jordi-beige/60 py-14 px-10 text-start shadow-xl shadow-black/5 backdrop-blur dark:bg-black/60">
